5eaa68288d6862e59a39abae5268cb98dcb54761
gary li <gary.li@signifyd.com>
2022-09-12 22:04:41 -0700

    fixed positioning issue for background anim

--- src/containers/Screens/HomeScreen/BackgroundAnimations/index.js
+++ src/containers/Screens/HomeScreen/BackgroundAnimations/index.js
@@ -9,8 +9,13 @@ import Fireworks from "./Fireworks";
 
 const useStyles = makeStyles({
     rootContainer: {
-        // transform: 'matrix3d(0.929, 0.369275, -0.0244082, 0, -0.367815, 0.928589, 0.0493373, 0, 0.0408843, -0.0368566, 0.998484, 0, 796.59, 299.838, 138.519, 1)',
-        // zIndex: zIndex.homeScreen.backgroundAnimation,
+        overflow: 'hidden',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        position: 'absolute',
+        height: '100%',
+        width: '100%',
     },
 });
 
