8a275695dd1144d9c4cf77434de71afae4c142f2
gary li <gary.li@signifyd.com>
2022-09-14 17:04:22 -0700

    prevent images from being dragged

--- src/containers/Screens/HomeScreen/Apps/App/index.js
+++ src/containers/Screens/HomeScreen/Apps/App/index.js
@@ -179,6 +179,7 @@ const App = ({
                                 e.preventDefault();
                             }
                         }}
+                        draggable={false}
                     />
                 )}
                 {descriptionActive && (
