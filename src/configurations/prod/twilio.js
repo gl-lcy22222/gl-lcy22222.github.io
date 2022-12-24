8b4e0ffb4d6d29aea27075b0a0688872f4970a63
gary li <gary.li@signifyd.com>
2022-12-23 17:44:58 -0800

    updated twilio authToken

--- src/configurations/prod/twilio.js
+++ src/configurations/prod/twilio.js
@@ -1,4 +1,4 @@
-const authToken = '791fd4d2c6b4830a372dff9e1db56938';
+const authToken = '8a317998e1906f85ba7fd68c42773f68';
 const accountSid = 'AC17e05bc52e2bb2ce6f8abc8f1730dd1a';
 const hostUrl = `https://api.twilio.com/2010-04-01/Accounts/${accountSid}/Messages.json`;
 const messagingServiceSid = 'MGf1288e25d8136e185d741117c732da01';
